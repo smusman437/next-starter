d312ff583868f00ed1a74af4c364a05fed0936e5
Muhammad Usman <50750473+smusman437@users.noreply.github.com>
2022-12-18 00:22:04 +0500

    update production url

--- constant/index.js
+++ constant/index.js
@@ -1,3 +1,4 @@
-const MAIN_URL = "http://localhost:8080/api";
+// const MAIN_URL = "http://localhost:8080/api";
+const MAIN_URL = "https://sage-alfajores-f14375.netlify.app/api";
 
 export const TODO_URL = `${MAIN_URL}/todos`;
