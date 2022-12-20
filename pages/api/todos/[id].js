ab4f82a993366fe4fcd018c81c7a9d266c9b87da
Muhammad Usman <50750473+smusman437@users.noreply.github.com>
2022-12-20 11:24:15 +0500

    remove logs

--- pages/api/todos/[id].js
+++ pages/api/todos/[id].js
@@ -1,7 +1,6 @@
 import db from "../../../db/DB_connection";
 
 export default async function handler(req, res) {
-    console.log("req.method:",req.method);
   let id, data;
   switch (req.method) {
     case "GET":
@@ -12,10 +11,8 @@ export default async function handler(req, res) {
       return res.status(200).send(data[0]);
     case "DELETE":
       id = req.query.id;
-      console.log("id:",id);
       await db.connect();
-      data = await db.query(`DELETE FROM todo where id = ${id}`);
-      console.log("data:",data);
+      await db.query(`DELETE FROM todo where id = ${id}`);
       await db.end();
       return res.status(200).send({ success: true });
   }
