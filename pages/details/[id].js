21dc373dd50709dca8eb680cc6bfc268d069d42e
Muhammad Usman <50750473+smusman437@users.noreply.github.com>
2022-12-22 16:38:58 +0500

    add shadow in card of detail page

--- pages/details/[id].js
+++ pages/details/[id].js
@@ -13,7 +13,10 @@ const Details = (props) => {
       <div className="container mt-4">
         <div className="row">
           <div className="col col-md-6 offset-md-3 d-flex justify-content-center">
-            <div className="card text-center" style={{ width: "18rem" }}>
+            <div
+              className="card text-center shadow-lg p-2 mb-5 bg-white rounded"
+              style={{ width: "18rem" }}
+            >
               <div className="card-body">
                 <h5 className="card-title">ID: {props.post.id}</h5>
                 <p className="card-text">TITLE: {props.post.title}</p>
